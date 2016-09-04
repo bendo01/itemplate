388d1d2e81e52bec3cd9f4fbb7edb9fe5ee8ca40
BennyLEP <bendo01@gmail.com>
2016-09-05 01:29:45 +0800

    finish action

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -15,4 +15,30 @@ $(document).ready(function(){
     $('.sub-menu-list > .show-menu').on('click', function(){
         $(this).siblings('.menu-list').toggleClass('hide');
     });
+    $('.trigger_tab').on('click', function(){
+        $(this).parent().siblings().removeClass('is-active');
+        $(this).parent().addClass('is-active');
+        const clickElement = $(this).attr('id');
+        const arrClickElementId = clickElement.split("_");
+        const tabTargetActivate = arrClickElementId[1]+'_'+arrClickElementId[2];
+        $('#'+tabTargetActivate).siblings().removeClass('is-active');
+        $('#'+tabTargetActivate).addClass('is-active');
+    });
+    $('.modal-button').on('click', function(){
+        var target = $(this).attr('data-target');
+        $('html').addClass('is-clipped');
+        $(target).addClass('is-active');
+    });
+    $('.modal-background, .modal-close').on('click', function(){
+        $('html').removeClass('is-clipped');
+        $(this).parent().removeClass('is-active');
+    });
+    $('.modal-card-head .delete, .modal-card-foot .button').on('click', function(){
+        $('html').removeClass('is-clipped');
+        $('.modal').removeClass('is-active');
+    });
+    //Notication Close Button
+    $('button.delete').on('click', function(){
+        $(this).parent().remove();
+    });
 });
