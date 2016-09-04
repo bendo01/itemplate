c5b5ca74a393225d132a61cf4be00413c462cba7
BennyLEP <bendo01@gmail.com>
2016-09-04 19:33:52 +0800

    rebuild using es2015

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+import $ from 'jquery';
+import jQuery from 'jquery';
+// export for others scripts to use
+window.$ = $;
+window.jQuery = jQuery;
+
+jQuery(document).ready(function ($) {
+    console.log('Ready');
+});
